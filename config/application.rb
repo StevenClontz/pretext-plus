c903e26fa9af8d61e80f7bf3bd05ba02d4667d25
Steven Clontz <steven.clontz@gmail.com>
2025-12-15 20:49:03 +0000

    set up uuids

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module PretextPlus
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+
+    # use uuids
+    config.generators do |generate|
+      generate.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
