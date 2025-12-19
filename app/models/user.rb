8172d1d9436351e4ccb49e5ebb11f9a68106517d
Steven Clontz <steven.clontz@gmail.com>
2025-12-19 22:01:07 +0000

    add registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,30 @@ class User < ApplicationRecord
   has_many :projects, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates_uniqueness_of :email_address
+
+  attr_accessor :invite_code
+
+  before_create :validate_invite_code
+  after_create_commit :expire_invite
+
+  private
+
+  def validate_invite_code
+    valid_invite = Invitation.where(recipient_user_id: nil).find_by(code: invite_code)
+    unless valid_invite.present?
+      errors.add(:invite_code, "is invalid")
+      throw :abort
+    end
+  end
+
+  def expire_invite
+    valid_invite = Invitation.where(recipient_user_id: nil).find_by(code: invite_code)
+    unless valid_invite.present?
+      errors.add(:invite_code, "is invalid")
+      throw :abort
+    end
+    valid_invite.update!(recipient_user_id: self.id)
+  end
 end
