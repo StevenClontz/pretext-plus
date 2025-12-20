2ce144def7295908eb60eeb4cf200519c39cb1c1
Steven Clontz <steven.clontz@gmail.com>
2025-12-20 23:03:23 +0000

    link to first doc, add small icon

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   resources :projects do
     get "share" => "projects#share"
   end
-  get "/projects/:id/article.html", to: redirect("/projects/%{id}/share")
+  get "projects/:id/article.html", to: redirect("/projects/%{id}/share")
+  get "*root/external/icon.svg", to: redirect("/icon-small.svg")
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
